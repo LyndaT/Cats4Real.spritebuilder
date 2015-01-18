c7782e368fb799ab9b4df4517bc286ed4cc2a196
oaruish <lyndatang2017@Lyndas-MacBook-Pro.local>
2015-01-18 00:00:15 -0500

    Damn Brown Block
    Added the damn brown block and implemented it.

--- Source/Gameplay.m
+++ Source/Gameplay.m
@@ -56,7 +56,7 @@ int numCake = 0;
     
     gameOverScreen = [CCBReader load:@"GameOver"];
     
-    currentLevelName=@"Levels/Level7";
+    currentLevelName=@"Levels/Level6";
     currentLevel = [CCBReader load:currentLevelName];
     _currentLevel = (Level *)currentLevel;
     
