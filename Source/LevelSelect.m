c5b44c89b5f93a4f62f0154c49210b421bbedd28
Jenny <jhl989@mit.edu>
2015-01-30 00:40:52 -0500

    Merge branch 'master' of https://github.com/oaruish/Cats4Real.spritebuilder
    Conflicts:
    	Source/Gameplay.m

--- Source/LevelSelect.m
+++ Source/LevelSelect.m
@@ -81,7 +81,6 @@
         [[NSUserDefaults standardUserDefaults] setInteger:1 forKey:@"highestlevel"];
         highestLevel = 1;
     }
-//    highestLevel = 18;
     int tempY=0;
     int tempX=0;
     for (int j = 1; j <=8; j++) {
